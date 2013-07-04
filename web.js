fd2b5207ac4e257a35c90c2f7d8e5fc9e4efb030
Antonio Coentrao <antonio.coentrao@gmail.com>
2013-07-04 22:50:38 +0000

    Adding content from index.html after correcion of app.get

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var fs = require('fs');
 
+var readFile = function(in){
+	var buf = new Buffer(255);
+	buf = fs.readFileSync(in);
+	return buf;
+}
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var inFile = 'index.html';
-  	var buf = new Buffer(255);
-	response.send(buf.toString(fs.readFileSync(inFile)));
+	var bufCont = readFile(inFile);
+	var strCon = bufCont.toString('utf8',0,bufCont.length);
+  	response.send(strCon);
 	//response.send('Hello');
 });
 
