e4bfd7961ad999c172154ddb9a0e6893eeb72f3b
Antonio Coentrao <antonio.coentrao@gmail.com>
2013-07-04 20:26:20 +0000

    Added content from index.html 2

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+//Reads content from input file
+var infile='index.html';
+var buf;
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString(fs.readFileSync(infile)));
 });
 
 var port = process.env.PORT || 5000;
