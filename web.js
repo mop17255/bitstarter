223035c4c2c70faf66fe6ea47078afb6d3fdd097
Antonio Coentrao <antonio.coentrao@gmail.com>
2013-07-15 23:33:48 +0000

    web.js with correct variable name.

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 	var inFile = "index.html";
 	var bufCont = new Buffer(50);
 	bufCont = fs.readFileSync(inFile,'utf8');
-	var strCon = buf.toString('utf8',0,bufCont.length);
+	var strCon = bufCont.toString('utf8',0,bufCont.length);
   	response.send(strCon);
 });
 
